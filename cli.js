ab7c97879e71af23890b7c0bd0787e78b065a979
GochoMugo <mugo@forfuture.co.ke>
2015-11-14 15:31:00 +0300

    Add support for input file
    Currently, we only use standard input for input. We might want
    to allow passing a path to a markdown file instead.

--- cli.js
+++ cli.js
@@ -3,7 +3,15 @@ var fs = require('fs')
 var concat = require('concat-stream')
 
 var md = require('./')
+var filepath = process.argv[2]
+var readStream
 
-process.stdin.pipe(concat(function (markdown) {
+if (filepath) {
+    readStream = fs.createReadStream(filepath)
+} else {
+    readStream = process.stdin
+}
+
+readStream.pipe(concat(function (markdown) {
   process.stdout.write(md(markdown.toString()))
 }))
